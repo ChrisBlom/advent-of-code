1c7aabd1d29bd6bd2ba46772f48e592f6fd7dd67
Chris Blom <chrisblom@chrisblom.net>
2025-12-06 12:41:07 +0100

    day 2025 day 06

---
+++ src/adventofcode/2025/day06.clj
@@ -0,0 +1,34 @@
+(ns adventofcode.2025.day06
+  (:require [clojure.string :as str]))
+
+(def ex "123 328  51 64
+ 45 64  387 23
+  6 98  215 314
+*   +   *   +  ")
+
+(defn parse-line [x]
+  (read-string (format "[%s]" x)))
+(defn parse [x]
+  (map parse-line (str/split-lines x)))
+
+
+(defn part-1 [ex]
+  (reduce + (map eval (apply  map  (comp reverse vector) (parse ex)))))
+
+
+(defn transpose [vs]
+  (apply map vector vs))
+
+(defn part-2 [x]
+  (let [lines (str/split-lines x)
+        nums (butlast lines)
+        ops (read-string (format "[%s]" (last lines)))
+        n (apply map (comp parse-long str/trim str) nums)
+        g (remove #{'(nil)}(partition-by nil? n))]
+    (eval (cons '+  (map cons
+                         ops
+                         g)))))
+
+(time
+ {:part-1 (part-1 (user/day-input))
+  :part-2 (part-2 (user/day-input))})
